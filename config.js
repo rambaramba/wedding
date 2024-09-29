4d53b1bb8942a118be1d41f57c3f5bfeff980025
rambaramba <yy992435@naver.com>
2024-09-30 07:24:29 +0900

    share image 변경

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 export const WEDDING_INVITATION_URL = "http://localhost:8000/";
 export const KAKAOTALK_API_TOKEN = "02c02d785ede652e7363ecd8d41e6051";
-export const KAKAOTALK_SHARE_IMAGE =
-  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+export const KAKAOTALK_SHARE_IMAGE = "src/images/ring_image.png";
 
 export const WEDDING_DATE = "2024년 11월 16일, 토요일 오후 2시";
 export const WEDDING_LOCATION = "루이비스컨벤션 송파문정 그레이스홀";
