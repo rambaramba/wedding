cf5cbddaaee64f2ca63df8c4773ad30b2e4c408a
rambaramba <yy992435@naver.com>
2024-09-30 07:37:05 +0900

    줄내림 방지2

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -19,7 +19,7 @@ const Wrapper = styled.div`
 
 const Title = styled.p`
   font-family: "nanum_hipi" !important;
-  font-size: 1.5rem;
+  font-size: 1.3 rem;
   color: var(--title-color);
   font-weight: bold;
   opacity: 0.85;
