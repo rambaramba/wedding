98ca195c98bc1e7a460f263e336743080d18074c
rambaramba <yy992435@naver.com>
2024-09-30 03:52:13 +0900

    오타 수정

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ export const KAKAOTALK_API_TOKEN = "02c02d785ede652e7363ecd8d41e6051";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2024년 11월 16일, 토요일 후 2시 00분";
+export const WEDDING_DATE = "2024년 11월 16일, 토요일 오후 2시 00분";
 export const WEDDING_LOCATION = "루이비스컨벤션 송파문정 그레이스홀";
 
 export const GROOM_NAME = "권덕근";
 export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
-export const GROOM_FATHER_NAME = "권정희";
+export const GROOM_FATHER_NAME = "권장희";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const GROOM_MOTHER_NAME = "신정경";
 export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
