e25f1b1262917ba80a662d84f8ae62ebc4c8614f
rambaramba <yy992435@naver.com>
2024-09-30 07:57:44 +0900

    title

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -29,12 +29,12 @@ const TitleWrapper = styled.div`
 `;
 
 const VideoBackground = styled.video`
-  width: 150%;
-  height: 50%;
+  width: 100%;
+  height: 65%;
 `;
 
 const WeddingInvitation = styled.p`
-  font-size: 3.0rem;
+  font-size: 1.8rem;
   opacity: 0.45;
   margin-bottom: 16px;
 `;
