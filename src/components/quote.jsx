87bc043d11490790b800c96a518f5d8f458ca886
rambaramba <yy992435@naver.com>
2024-09-30 06:30:45 +0900

    quote margin

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -15,7 +15,7 @@ const Wrapper = styled.div`
 
 const Content = styled.span`
   display: block;
-  margin: 0 auto;
+  margin: 0 ;
   font-size: 1.3rem;
   font-family: "mom_to_daughter" !important;
   text-align: center;
@@ -39,6 +39,7 @@ const Quote = () => {
     <Wrapper>
       <Image src={Flower} data-aos="fade-up" />
       <Content data-aos="fade-up">
+        <br />
         사랑한다는 말은 설레고 
         <br />
         포옹은 따뜻하고
