5b76dccccefa6bffd2bcf62b470216b99f31e685
rambaramba <yy992435@naver.com>
2024-09-30 02:44:04 +0900

    javascript key

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const WEDDING_INVITATION_URL = "http://localhost:8000/";
-export const KAKAOTALK_API_TOKEN = "a5dd5018ab2d7b36e1805be517361f1e";
+export const KAKAOTALK_API_TOKEN = "02c02d785ede652e7363ecd8d41e6051";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
