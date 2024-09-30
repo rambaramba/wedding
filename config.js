09823152e99f087adca58299251b949db5a59d7b
rambaramba <yy992435@naver.com>
2024-09-30 13:32:38 +0900

    계좌번호 수정

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ export const WEDDING_DATE = "2024년 11월 16일, 토요일 오후 2시";
 export const WEDDING_LOCATION = "루이비스컨벤션 송파문정 그레이스홀";
 
 export const GROOM_NAME = "권덕근";
-export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_ACCOUNT_NUMBER = "국민은행 354802-04-025579";
 export const GROOM_FATHER_NAME = "권장희";
-export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "농협은행 016-02-178683";
 export const GROOM_MOTHER_NAME = "신정경";
-export const GROOM_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_MOTHER_ACCOUNT_NUMBER = "국민은행 831-21-0448-684";
 
 export const BRIDE_NAME = "정바램";
-export const BRIDE_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_ACCOUNT_NUMBER = "신한은행 110-488-925786";
 export const BRIDE_FATHER_NAME = "정창문";
 export const BRIDE_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const BRIDE_MOTHER_NAME = "이전숙";
-export const BRIDE_MOTHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "농협은행 857-02-031050";
