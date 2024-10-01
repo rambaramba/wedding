d5c7cb7b605bfc782c0f6c499a9e15afaf4f21cd
rambaramba <yy992435@naver.com>
2024-10-02 00:13:18 +0900

    사진 수정

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -3,16 +3,27 @@ import ImageGallery from "react-image-gallery";
 import { Divider } from "antd";
 import styled from "styled-components";
 
-import GalleryPhoto1 from "../assets/GalleryPhoto1.jpg";
-import GalleryPhoto2 from "../assets/1SY00256.jpg";
-import GalleryPhoto3 from "../assets/1SY00681.jpg";
-import GalleryPhoto4 from "../assets/1SY00903.jpg";
-import GalleryPhoto5 from "../assets/1SY01124+.jpg";
-import GalleryPhoto6 from "../assets/1SY01418+.jpg";
-import GalleryPhoto7 from "../assets/a1SY00548.jpg";
-import GalleryPhoto8 from "../assets/AM4I7612.jpg";
-import GalleryPhoto9 from "../assets/ba1SY01458.jpg";
-
+import GalleryPhoto1  from "../assets/GalleryPhoto1.jpg";
+import GalleryPhoto2  from "../assets/GalleryPhoto2.jpg";
+import GalleryPhoto3  from "../assets/GalleryPhoto3.jpg";
+import GalleryPhoto4  from "../assets/GalleryPhoto4.jpg";
+import GalleryPhoto5  from "../assets/GalleryPhoto5.jpg";
+import GalleryPhoto6  from "../assets/GalleryPhoto6.jpg";
+import GalleryPhoto7  from "../assets/GalleryPhoto7.jpg";
+import GalleryPhoto8  from "../assets/GalleryPhoto8.jpg";
+import GalleryPhoto9  from "../assets/GalleryPhoto9.jpg";
+import GalleryPhoto10 from "../assets/GalleryPhoto10.jpg";
+import GalleryPhoto11 from "../assets/GalleryPhoto11.jpg";
+import GalleryPhoto12 from "../assets/GalleryPhoto12.jpg";
+import GalleryPhoto13 from "../assets/GalleryPhoto13.jpg";
+import GalleryPhoto14 from "../assets/GalleryPhoto14.jpg";
+import GalleryPhoto15 from "../assets/GalleryPhoto15.jpg";
+import GalleryPhoto16 from "../assets/GalleryPhoto16.jpg";
+import GalleryPhoto17 from "../assets/GalleryPhoto17.jpg";
+import GalleryPhoto18 from "../assets/GalleryPhoto18.jpg";
+import GalleryPhoto19 from "../assets/GalleryPhoto19.jpg";
+import GalleryPhoto20 from "../assets/GalleryPhoto20.jpg";
+import GalleryPhoto21 from "../assets/GalleryPhoto21.jpg";
 
 
 const Wrapper = styled.div`
@@ -67,6 +78,54 @@ const images = [
   {
     original: GalleryPhoto9,
     thumbnail: GalleryPhoto9,
+  },  
+  {
+    original: GalleryPhoto10,
+    thumbnail: GalleryPhoto10,
+  },
+  {
+    original: GalleryPhoto11,
+    thumbnail: GalleryPhoto11,
+  },
+  {
+    original: GalleryPhoto12,
+    thumbnail: GalleryPhoto12,
+  },
+  {
+    original: GalleryPhoto13,
+    thumbnail: GalleryPhoto13,
+  },
+  {
+    original: GalleryPhoto14,
+    thumbnail: GalleryPhoto14,
+  },
+  {
+    original: GalleryPhoto15,
+    thumbnail: GalleryPhoto15,
+  },
+  {
+    original: GalleryPhoto16,
+    thumbnail: GalleryPhoto16,
+  },
+  {
+    original: GalleryPhoto17,
+    thumbnail: GalleryPhoto17,
+  },
+  {
+    original: GalleryPhoto18,
+    thumbnail: GalleryPhoto18,
+  },
+  {
+    original: GalleryPhoto19,
+    thumbnail: GalleryPhoto19,
+  },
+  {
+    original: GalleryPhoto20,
+    thumbnail: GalleryPhoto20,
+  },
+  {
+    original: GalleryPhoto21,
+    thumbnail: GalleryPhoto21,
   },  
 ];
 
