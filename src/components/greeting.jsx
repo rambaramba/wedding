3d5259de730988b2bbd2db95d14835321a2ca7bf
rambaramba <yy992435@naver.com>
2024-10-02 23:24:10 +0900

    그리팅 폰트 사이즈

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -29,7 +29,7 @@ const Title = styled.p`
 
 const Content = styled.p`
   font-family: "nanum_lefthand"  !important;
-  font-size: 1.2 rem;
+  font-size: 1.1 rem;
   line-height: 1.75;
   opacity: 0.75;
   margin-bottom: 16px;
